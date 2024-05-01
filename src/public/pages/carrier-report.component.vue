33e7df0a0b21b44b72bc986a4b271dc6ae3fd106
ELÍAS TORRES ESPINOZA <102787891+EliasTorresEzpinoza@users.noreply.github.com>
2024-04-30 22:32:01 -0500

    feat

---
+++ src/public/pages/carrier-report.component.vue
@@ -0,0 +1,21 @@
+<script>
+import ReportView from "../components/report-view.component.vue";
+import ReportDriver from "../components/report-driver.component.vue";
+import Sidebar from "../components/sidebar.component.vue";
+
+export default {
+  name: "carrier-report",
+  components: {ReportDriver, ReportView,Sidebar}
+}
+</script>
+
+<template>
+  <div class="container">
+    <sidebar></sidebar>
+    <report-driver></report-driver>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/public/pages/organization-shipment.component.vue
@@ -0,0 +1,24 @@
+<script>
+import Sidebar from "../components/sidebar.component.vue";
+import OrganizationBusinessman from "../../service-execution/components/organization-businessman.component.vue";
+
+export default {
+  name: "organization-shipment",
+  components: {OrganizationBusinessman,Sidebar},
+
+}
+</script>
+
+<template>
+  <div class="container">
+    <sidebar></sidebar>
+    <organization-businessman></organization-businessman>
+  </div>
+
+</template>
+
+<style>
+.container{
+  display: flex;
+}
+</style>
